8a857258a0a450b61650c7c0c3b01edb52e97e47
xuewei.wang <thoughtworks.w.wang@extaccount.com>
2021-07-11 11:04:02 +0800

    xw new activity, git image add

--- app/src/main/java/com/thoughtworks/snowtryagain/MainActivity.java
+++ app/src/main/java/com/thoughtworks/snowtryagain/MainActivity.java
@@ -1,14 +1,26 @@
 package com.thoughtworks.snowtryagain;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button0);
+        button.setOnClickListener(v -> openNewActivity());
+    }
+
+    public void openNewActivity() {
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/thoughtworks/snowtryagain/NewActivity.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.snowtryagain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
